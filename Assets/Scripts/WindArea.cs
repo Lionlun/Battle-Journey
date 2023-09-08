ca42dcff03fb862a95d345632bf5884b235bb357
Leo Lunev <loganlev@bk.ru>
2023-09-08 11:29:41 +0300

    Feat: Добавить подсчет дистанции от WindArea до Wall, для разворота объекта к дальней стене.

--- Assets/Scripts/WindArea.cs
+++ Assets/Scripts/WindArea.cs
@@ -1,11 +1,43 @@
+using System;
 using UnityEngine;
 
 public class WindArea : MonoBehaviour
 {	
 	[SerializeField] private float strength;
+	[SerializeField] private LayerMask layerMask;
+	private float raycastRange = 15f;
 
+	private void FixedUpdate()
+	{
+		RotateWindTowardsDistantWall();
+	}
 	public Vector3 GetDirectionForce()
 	{
 		return transform.right * strength;
+	}
+
+	private void RotateWindTowardsDistantWall()
+	{
+		if (GetDistanceToWall() < 2)
+		{
+			transform.RotateAround(transform.position, transform.up, 180f);
+		}
+	}
+
+	private float GetDistanceToWall() // ToDo сделать отдельный класс, который рассчитывает дистанцию до объектов и добавить его к Laser и Wind Area
+	{
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.right, out hit, raycastRange, layerMask, QueryTriggerInteraction.Ignore))
+		{
+			if (hit.collider.gameObject.GetComponent<Wall>() != null)
+			{
+				Debug.Log("Wind " + hit.distance);
+				Debug.DrawLine(transform.position, transform.right, Color.red);
+				return hit.distance;
+			}
+		}
+
+		return Int32.MaxValue;
 	}
 }
