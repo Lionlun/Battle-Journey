d800fe0661425faf7b7791ed6cdc0ccec006dd40
Leo Lunev <loganlev@bk.ru>
2023-09-08 11:40:48 +0300

    Feat: Добавить FloorTrap, FloorFlash.

---
+++ Assets/Scripts/FloorFlash.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class FloorFlash : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    private Color originalColor;
+	private float numberOfFlashes = 3;
+    private int flashTime = 500;
+
+	private void Start()
+	{
+		meshRenderer = GetComponent<MeshRenderer>();
+        originalColor = meshRenderer.material.color;
+	}
+
+	public async Task Flash()
+    {
+		for (int i = 0; i < numberOfFlashes; i++)
+		{
+			meshRenderer.material.color = Color.red;
+			await Task.Delay(flashTime);
+			meshRenderer.material.color = originalColor;
+			await Task.Delay(flashTime);
+		}
+	}
+
+	public async Task ActivateTrap()
+	{
+		await Flash();
+		this.gameObject.SetActive(false);
+	}
+
+	public void ResetTrap()
+	{
+		this.gameObject.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/FloorTrap.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(MazeRenderer))]
+public class FloorTrap : MonoBehaviour
+{
+	private List<MazeCellObject> cells = new List<MazeCellObject>();
+	private MazeRenderer mazeRenderer;
+	private int secondsToReturnFloor = 2;
+
+	private float minimumTimeToTriggerTrap = 5;
+	private float maximumTimeToTriggerTrap = 30;
+
+	private IEnumerator currentRoutine;
+
+	private async void Start()
+	{
+		mazeRenderer = GetComponent<MazeRenderer>();
+		await Task.Delay(1000); // ToDo Сделать нормально, временное решение Дожидаемся, когда сформируется список. Т.Е делаем Await, либо Event
+
+		FindAllMazeCells();
+		currentRoutine = TrapActivationRoutine();
+		StartCoroutine(currentRoutine);
+	}
+
+	private void FindAllMazeCells()
+	{
+		foreach (Transform child in transform)
+		{
+			if (child.gameObject.GetComponent<MazeCellObject>() != null)
+			{
+				cells.Add(child.gameObject.GetComponent<MazeCellObject>());
+			}
+		}
+	}
+	
+	private void StartFloorTrap()
+	{
+		var cellsWithWall = FindAllCellsWithWall();
+		var random = Random.Range(0, cellsWithWall.Count-1);
+		var cellFlash = cellsWithWall[random].GetComponentInChildren<FloorFlash>();
+		ManageTrapOperation(cellFlash);
+	}
+
+	private List<MazeCellObject> FindAllCellsWithWall()
+	{
+		var cellsWithWall = new List<MazeCellObject>();
+
+		foreach (var cell in MazeCellsDictionary.Cells.Values)
+		{
+			if (cell.CheckWallsPresence())
+			{
+				cellsWithWall.Add(cell);
+			}
+		}
+		return cellsWithWall;
+	}
+
+	private async void ManageTrapOperation(FloorFlash floor)
+	{
+		await floor.ActivateTrap();
+		await Task.Delay(secondsToReturnFloor*1000);
+		floor.ResetTrap();
+	}
+
+	private IEnumerator TrapActivationRoutine()
+	{
+		while(true)
+		{
+			var randomTime = Random.Range(minimumTimeToTriggerTrap, maximumTimeToTriggerTrap);
+			yield return new WaitForSeconds(randomTime);
+			StartFloorTrap();
+			yield return null;
+		}
+	}
+}
