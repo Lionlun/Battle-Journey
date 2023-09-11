267425dcdc7112124ffbaf2bb888dffed43b4fc8
Leo Lunev <loganlev@bk.ru>
2023-09-11 19:57:55 +0300

    Feat: Добавить событие OnSceneReload.

--- Assets/Scripts/Managers/SceneReload.cs
+++ Assets/Scripts/Managers/SceneReload.cs
@@ -1,10 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public static class SceneReload
 {
-    public static void RestartCurrentScene()
+	public static Action OnSceneReload;
+
+	public static void RestartCurrentScene()
     {
+		OnSceneReload?.Invoke();
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 }
