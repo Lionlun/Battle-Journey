1b1080b83d33d75f6e1fd99751834ce37e5364aa
Leo Lunev <loganlev@bk.ru>
2023-09-12 12:40:15 +0300

    Feat: Добавить кулдаун для Knockback.

--- Assets/Scripts/Player/Knockback.cs
+++ Assets/Scripts/Player/Knockback.cs
@@ -3,15 +3,33 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Knockback : MonoBehaviour
 {
-	Rigidbody rb;
+	private Rigidbody rb;
+	private float knockbackCooldown = 0;
+	private float knockbackCooldownRefresh = 1;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody>();
 	}
+	private void Update()
+	{
+		CheckCooldown();
+	}
 	public void KnockBack(Vector3 objectPosition, float knockbackForce)
 	{
-		var direction = transform.position - objectPosition;
-		rb.AddForce(direction * knockbackForce, ForceMode.Force);
+		if (knockbackCooldown <= 0)
+		{
+			var direction = transform.position - objectPosition;
+			rb.AddForce(direction * knockbackForce, ForceMode.Impulse);
+			knockbackCooldown = knockbackCooldownRefresh;
+		}
+	}
+	
+	private void CheckCooldown()
+	{
+		if (knockbackCooldown >= 0)
+		{
+			knockbackCooldown -= Time.deltaTime;
+		}
 	}
 }
