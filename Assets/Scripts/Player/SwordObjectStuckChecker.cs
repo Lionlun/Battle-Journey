2f7df6f4cd395979556fbe59bf3e97dba5104193
Leo Lunev <loganlev@bk.ru>
2023-09-12 13:23:38 +0300

    Feat: Создать отдельный класс для проверки состояния Sword (stuck/unstuck).

---
+++ Assets/Scripts/Player/SwordObjectStuckChecker.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SwordObjectStuckChecker : MonoBehaviour
+{
+	public bool IsWallStuck { get; set; }
+	public bool IsEnemyStuck { get; set; }
+
+
+	private void ObjectStuck<T>(T type)
+	{
+		if (type is Enemy)
+		{
+			IsWallStuck = false;
+			IsEnemyStuck = true;
+		}
+		else if (type is Wall)
+		{
+			IsWallStuck = true;
+			IsEnemyStuck = false;
+		}
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.GetComponent<Enemy>() != null)
+		{
+			var enemy = other.gameObject.GetComponent<Enemy>();
+			ObjectStuck(enemy);
+		}
+
+		if (other.gameObject.GetComponent<Wall>() != null)
+		{
+			var wall = other.gameObject.GetComponent<Wall>();
+			ObjectStuck(wall);
+		}
+	}
+}
