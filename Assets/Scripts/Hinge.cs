fe458fccfd05ccf031bf9d16d0d8574736e0af34
Leo Lunev <loganlev@bk.ru>
2023-09-13 15:01:42 +0300

    Feat: Добавить свойство IsActive к классу Hinge.

--- Assets/Scripts/Hinge.cs
+++ Assets/Scripts/Hinge.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 
 public class Hinge: MonoBehaviour
 {
+	public bool IsActive { get; set; }
+
     public void Activate()
     {
-        gameObject.SetActive(true);
+		IsActive = true;
+		Debug.Log("Hinge activated");
+		gameObject.SetActive(true);
     }
 	public void Deactivate()
 	{
+		Debug.Log("Hinge DEactivated");
+		IsActive = false;
 		gameObject.SetActive(false);
 	}
 }
